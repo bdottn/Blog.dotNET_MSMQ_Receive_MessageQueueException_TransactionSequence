bd8f8276e0abff1bc1ed7bbc45d9fca80133a858
Echo Lee <bdottn.lee+git@gmail.com>
2017-08-21 16:53:43 +0800

    建立 QueueService
    1.　加入建構式：QueueService
    　1.1　queuePath：訊息佇列用戶端存取名稱
    
    2.　加入方法：Send
    3.　加入方法：Receive

---
+++ Service/QueueService.cs
@@ -0,0 +1,41 @@
+﻿using Model;
+using System;
+using System.Messaging;
+
+namespace Service
+{
+    public sealed class QueueService
+    {
+        private readonly MessageQueue messageQueue;
+        private readonly MessageQueueTransaction transaction;
+
+        public QueueService(string queuePath)
+        {
+            this.messageQueue = new MessageQueue(queuePath);
+
+            this.transaction = new MessageQueueTransaction();
+        }
+
+        public void Send(Student student)
+        {
+            this.transaction.Begin();
+
+            this.messageQueue.Send(student, this.transaction);
+
+            this.transaction.Commit();
+        }
+
+        public Student Receive(TimeSpan timeout)
+        {
+            this.messageQueue.Formatter = new XmlMessageFormatter(new Type[] { typeof(Student), });
+
+            this.transaction.Begin();
+
+            var message = this.messageQueue.Receive(timeout, this.transaction);
+
+            this.transaction.Commit();
+
+            return (Student)message.Body;
+        }
+    }
+}
